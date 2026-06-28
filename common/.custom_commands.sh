67573e1ab52d56c245c83a6f191a089fa104cbc4
reep_c <reep_c@outlook.com>
2026-06-28 20:47:51 +0800

    [refactor] Remove .custom_commands.sh in the root and put inside common. Add function to show ssh config.

--- common/.custom_commands.sh
+++ common/.custom_commands.sh
@@ -1,6 +1,7 @@
 # Custom Function
 # Aliases now live in ~/.zsh_alias.zsh (stowed from common/.zsh_alias.zsh).
 function delete_tmux_session {
-    tmux kill-session -a
-    tmux rename-session 0
+  tmux kill-session -a
+  tmux rename-session 0
 }
+
